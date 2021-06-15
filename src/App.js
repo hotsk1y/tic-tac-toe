67a50082cbb56c8665ee35d89a3583aa56bbc9cf
Aleksandr Hotskiy <hotskyi.aleksandr@gmail.com>
2021-06-15 20:39:16 +0300

    ai

--- src/App.js
+++ src/App.js
@@ -1,40 +1,50 @@
 import React, { useState } from 'react'
-import './App.css';
+import './App.css'
 import Game from './components/Game/Game'
 import Menu from './components/Menu/Menu'
 
 function App() {
   const [menuActive, setMenuActive] = useState(false)
-  const [isAgainstTheComputer, setIsAgainstTheComputer] = useState(false)
+  const [isAgainstTheComputer, setIsAgainstTheComputer] = useState(true)
   const [xIsRunMenu, setxIsRunMenu] = useState(true)
   const [xIsRun, setxIsRun] = useState(xIsRunMenu)
   const [turn, setTurn] = useState([])
 
+  let disabledInput = false;
+  if (turn.length >= 1) {
+    disabledInput = true
+  } else {
+    disabledInput = false
+  }
+
   return (
     <div className="main-page">
       <div className="menu__settings-logo" onClick={() => setMenuActive(!menuActive)}>
-        <img src="images/settings.svg" alt=""/>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Windows_Settings_app_icon.png/768px-Windows_Settings_app_icon.png" alt="" />
       </div>
-      <div className={menuActive ? "main-menu active" : "main-menu"}>
-        <Menu menuActive={menuActive} 
-        setMenuActive={setMenuActive} 
-        setIsAgainstTheComputer={setIsAgainstTheComputer}
-        isAgainstTheComputer={isAgainstTheComputer}
-        xIsRunMenu={xIsRunMenu}
-        setxIsRunMenu={setxIsRunMenu}
-        xIsRun={xIsRun}
-        setxIsRun={setxIsRun}
-        turn={turn} />
+      <div className={menuActive ? 'main-menu active' : 'main-menu'}>
+        <Menu
+          menuActive={menuActive}
+          disabledInput={disabledInput}
+          setMenuActive={setMenuActive}
+          setIsAgainstTheComputer={setIsAgainstTheComputer}
+          isAgainstTheComputer={isAgainstTheComputer}
+          xIsRunMenu={xIsRunMenu}
+          setxIsRunMenu={setxIsRunMenu}
+          xIsRun={xIsRun}
+          setxIsRun={setxIsRun}
+        />
       </div>
-      <Game 
+      <Game
         isAgainstTheComputer={isAgainstTheComputer}
         xIsRunMenu={xIsRunMenu}
         xIsRun={xIsRun}
         setxIsRun={setxIsRun}
         turn={turn}
-        setTurn={setTurn} />        
+        setTurn={setTurn}
+      />
     </div>
-  );
+  )
 }
 
 export default App;
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -7,25 +7,24 @@ const Menu = ({ setMenuActive,
   setxIsRun,
   xIsRunMenu,
   setxIsRunMenu,
-  turn
+  disabledInput
 }) => {
 
-  let disabledInput = false
-  turn.length >=1 ? disabledInput = true : disabledInput = false
-
   return (
-    <div className="menu">      
+    <div className="menu">
       <div className="close-menu__btn" onClick={() => setMenuActive(false)}>X</div>
       <div className="main-menu__content">
-      <div className="title"><h1>Tic Tac Toe Settings</h1></div>
+        <div className="title"><h1>Tic Tac Toe Settings</h1></div>        
         <div className="content-item chekbox-content__item">
           <label htmlFor="computerTurn">Player VS Bot: </label>
           <input id="computerTurn" type="checkbox" disabled={disabledInput} checked={isAgainstTheComputer} onChange={(e) =>
             setIsAgainstTheComputer(e.target.checked)} />
         </div>
         <div className="content-item chekbox-content__item">
-          <label htmlFor="xIsRun">X runs first: </label> 
-          <input className='chekbox-content__item' id="xIsRun" type="checkbox" disabled={disabledInput} checked={xIsRunMenu}
+          <label htmlFor="xIsRun">X runs first: </label>
+          <input className='chekbox-content__item' id="xIsRun" type="checkbox"
+            disabled={disabledInput}
+            checked={xIsRunMenu}
             onChange={(e) => {
               setxIsRunMenu(e.target.checked)
               setxIsRun(e.target.checked)
